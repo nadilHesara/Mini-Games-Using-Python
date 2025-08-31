ff8ce89cf095a2d3e15e59aa990663d6db2f7803
nadilHesara <nadilhesara@gmail.com>
2025-08-31 20:53:31 +0530

    recurisive and back tracking done

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -1,7 +1,7 @@
 def find_next_empty_cell(puzzle):
     for r in range(9):
         for c in range(9):
-            if puzzle[r][c] == -1:
+            if puzzle[r][c] == '#':
                 return r,c
     
     return None, None  # puzzle is fully filled
@@ -40,8 +40,31 @@ def solve_sudoku(puzzle):
         if is_valid_number(puzzle, guess, row, col):
             puzzle[row][col] = guess
 
+            #recursive call go to the very end of the puzzle until it solves
             if solve_sudoku(puzzle):
                 return True
 
+        #back tracking - try with a new guess after reseting the puzzle  
+        puzzle[row][col] = '#'  #reseting the guess back to empty cell
+
+    return False
+
+
+if __name__ == '__main__':
+    puzzle = [
+        [3, 9, '#', '#', 5, '#', '#', '#', '#'],
+        ['#', '#', '#', 2, '#', '#', '#', '#', 5],
+        ['#','#', '#', 7, 1, 9, '#', 8, '#'],
+        ['#', 5, '#', '#', 6, 8, '#', '#', '#'],
+        [2, '#', 6, '#', '#', 3, '#', '#', '#'],
+        ['#', '#', '#', '#', '#', '#', '#', '#', 4],
+        [5, '#', '#', '#', '#', '#', '#', '#', '#'],
+        [6, 7, '#', 1, '#', 5, '#', 4, '#'],
+        [1, '#', 9, '#', '#', '#', 2, '#', '#'],
+
+    ]
+
+    print(solve_sudoku(puzzle))
+    print(puzzle)
 
     
